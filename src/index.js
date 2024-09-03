54b7c8c0cbe310a8d89200782cc23ed8ba7abb41
SaraToth <saratoth101@gmail.com>
2024-09-03 17:57:30 +0900

    Create event listeners that append or remove a class list show which will be associated with the display property in css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import "./styles.css";
+
+const dropdownBtn = document.querySelector(".btn-dropdown");
+const dropdownContent = document.querySelector(".dropdown-content");
+const dropdownLinks = document.querySelector(".nav-menu");
+
+
+//Displays dropdown content
+dropdownBtn.addEventListener("click", () => {
+    dropdownContent.classList.add("show");
+});
+
+//Hides dropdown content if user clicks away
+window.addEventListener("click", (e) => {
+    if(e.target != dropdownBtn && e.target != dropdownLinks) {
+        dropdownContent.classList.remove("show");
+    };
+});
