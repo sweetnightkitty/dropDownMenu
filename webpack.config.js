f2af808de5f0c3fa66edde1f9ead7c221f72c4ae
SaraToth <saratoth101@gmail.com>
2024-09-03 16:15:00 +0900

    Change webpack file name to remove typo in naming

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require ("path");
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
@@ -9,21 +9,22 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         clean: true,
     },
+
     devtool: "eval-source-map",
     devServer: {
         watchFiles: ["./src/template.html"],
     },
+
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/template.html",
         }),
     ],
+
     module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-        ],
+        rules: [{
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        }],
     },
 };
